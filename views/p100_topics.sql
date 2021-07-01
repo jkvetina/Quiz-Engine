8244a6ece102bf05071ebf3e4c4ac555b6db902b
Jan Kvetina <jan.kvetina@gmail.com>
2021-07-01 19:57:17 +0200

    Views for homepage

--- views/p100_topics.sql
+++ views/p100_topics.sql
@@ -3,18 +3,27 @@ SELECT
     t.topic_id,
     t.topic_name            AS name,
     --
-    NULL                    AS supplemental,
-    q.count_                AS counter,
+    q.questions || ' questions, ' || q.explanations || ' explanations' AS supplemental,
+    --
+    q.tests                 AS counter,
     --
     CASE WHEN t.topic_id = apex.get_item('$TEST_GROUP')
         THEN 'bold'
         END AS css_class
 FROM quiz_topics t
-LEFT JOIN (
+JOIN (
     SELECT
         t.test_topic,
-        COUNT(*)            AS count_
-    FROM quiz_tests t
+        COUNT(q.question_id)                                            AS questions,
+        SUM(CASE WHEN q.explanation     IS NOT NULL THEN 1 ELSE 0 END)  AS explanations,
+        COUNT(DISTINCT t.test_id)                                       AS tests
+    FROM quiz_questions q
+    JOIN quiz_tests t
+        ON t.test_id        = q.test_id
+    WHERE (
+            t.dedicated_to      IS NULL
+            OR t.dedicated_to   = sess.get_user_id()
+        )
     GROUP BY t.test_topic
 ) q
     ON q.test_topic         = t.topic_id
---
+++ views/p110_bookmarked.sql
@@ -0,0 +1,40 @@
+CREATE OR REPLACE VIEW p100_tests AS
+SELECT
+    t.test_id,
+    t.test_name,
+    --
+    t.questions || ' questions, ' || t.explanations || ' explanations, ' ||
+        NVL(FLOOR(100 * (t.is_correct / t.questions)), 0) || '% completed'
+        AS supplemental,
+    --
+    CASE WHEN t.is_correct > 0
+        THEN NVL(FLOOR(100 - 100 * (t.is_bookmarked / t.questions)), 0) || '%'
+        END AS count_,
+    --
+    CASE WHEN t.test_id = apex.get_item('$TEST_ID')
+        THEN 'bold'
+        END AS css_class
+FROM (
+    SELECT
+        t.test_id,
+        t.test_name,
+        COUNT(q.question_id)                                            AS questions,
+        SUM(CASE WHEN q.explanation     IS NOT NULL THEN 1 ELSE 0 END)  AS explanations,
+        SUM(CASE WHEN a.is_correct      = 'Y'       THEN 1 ELSE 0 END)  AS is_correct,
+        SUM(CASE WHEN a.is_bookmarked   = 'Y'       THEN 1 ELSE 0 END)  AS is_bookmarked,
+        t.dedicated_to
+    FROM quiz_questions q
+    JOIN quiz_tests t
+        ON t.test_id        = q.test_id
+    LEFT JOIN quiz_attempts a
+        ON a.user_id        = sess.get_user_id()
+        AND a.test_id       = q.test_id
+        AND a.question_id   = q.question_id
+    WHERE t.test_topic      = apex.get_item('$TEST_GROUP')
+        AND (
+            t.dedicated_to      IS NULL
+            OR t.dedicated_to   = sess.get_user_id()
+        )
+    GROUP BY t.test_id, t.test_name, t.dedicated_to
+) t
+ORDER BY t.dedicated_to NULLS FIRST, t.test_id;
