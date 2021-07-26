540b3cd0bee0fcd46b850ee93e179fdc3310b751
Jan Kvetina <jan.kvetina@gmail.com>
2021-07-26 12:03:50 +0200

    New lines, image if exists

--- apex/f666/application/pages/page_00100.sql
+++ apex/f666/application/pages/page_00100.sql
@@ -25,7 +25,7 @@ wwv_flow_api.create_page(
 '}'))
 ,p_page_template_options=>'#DEFAULT#'
 ,p_last_updated_by=>'QUIZ_DEV'
-,p_last_upd_yyyymmddhh24miss=>'20210705190929'
+,p_last_upd_yyyymmddhh24miss=>'20210726095901'
 );
 wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(143432731519904421)
@@ -46,8 +46,11 @@ wwv_flow_api.create_page_plug(
 '    WHERE t.topic_id        = apex.get_item(''$TEST_GROUP'')',
 '        AND t.badge_name    IS NOT NULL',
 ') LOOP',
+'    IF c.badge_image IS NOT NULL THEN',
 '    htp.p(''<img src="#APP_IMAGES#'' || c.badge_image || ''" width="180" style="float: left; clear: none; padding: 2rem 3rem 2rem 2rem;" />'');',
-'    htp.p(''<p style="line-height: 150%;">'' || c.note || ''</p>'');',
+'    END IF;',
+'    --',
+'    htp.p(''<p style="line-height: 150%;">'' || REPLACE(c.note, CHR(10) || CHR(10), ''</p><p style="line-height: 150%;">'') || ''</p>'');',
 'END LOOP;',
 ''))
 ,p_plug_source_type=>'NATIVE_PLSQL'
@@ -82,6 +85,7 @@ wwv_flow_api.create_page_plug(
 ,p_plug_display_point=>'BODY'
 ,p_query_type=>'TABLE'
 ,p_query_table=>'P100_TESTS'
+,p_query_order_by=>'test_id'
 ,p_include_rowid_column=>false
 ,p_plug_source_type=>'NATIVE_JQM_LIST_VIEW'
 ,p_plug_query_num_rows=>30
