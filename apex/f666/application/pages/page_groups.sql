1bbec1144b7e4c679903c4d3b1553ab7f9d8f32d
Jan Kvetina <jan.kvetina@gmail.com>
2021-09-19 15:33:56 +0200

    New page group added

--- apex/f666/application/pages/page_groups.sql
+++ apex/f666/application/pages/page_groups.sql
@@ -12,6 +12,10 @@ wwv_flow_api.component_begin (
 ,p_default_owner=>'DEV'
 );
 wwv_flow_api.create_page_group(
+ p_id=>wwv_flow_api.id(157555888025214138)
+,p_group_name=>'PLAN'
+);
+wwv_flow_api.create_page_group(
  p_id=>wwv_flow_api.id(142404407822658913)
 ,p_group_name=>'QUIZ'
 );
