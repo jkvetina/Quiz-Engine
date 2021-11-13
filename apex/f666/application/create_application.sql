6154891830d0be67a38e20034059c38c7672d7b9
Jan Kvetina <jan.kvetina@gmail.com>
2021-11-13 07:47:46 +0100

    Just to keep git clean

--- apex/f666/application/create_application.sql
+++ apex/f666/application/create_application.sql
@@ -4,8 +4,8 @@ begin
 --     FLOW: 666
 --   Manifest End
 wwv_flow_api.component_begin (
- p_version_yyyy_mm_dd=>'2020.10.01'
-,p_release=>'20.2.0.00.20'
+ p_version_yyyy_mm_dd=>'2021.04.15'
+,p_release=>'21.1.6'
 ,p_default_workspace_id=>123132524645685789
 ,p_default_application_id=>666
 ,p_default_id_offset=>0
@@ -53,7 +53,7 @@ wwv_flow_api.create_flow(
 ,p_substitution_string_01=>'APP_NAME'
 ,p_substitution_value_01=>'Quiz Engine'
 ,p_last_updated_by=>'QUIZ_DEV'
-,p_last_upd_yyyymmddhh24miss=>'20210921201908'
+,p_last_upd_yyyymmddhh24miss=>'20211113064051'
 ,p_file_prefix => nvl(wwv_flow_application_install.get_static_app_file_prefix,'')
 ,p_files_version=>11
 ,p_ui_type_name => null
--- apex/f666/application/pages/page_00125.sql
+++ apex/f666/application/pages/page_00125.sql
@@ -4,8 +4,8 @@ begin
 --     PAGE: 00125
 --   Manifest End
 wwv_flow_api.component_begin (
- p_version_yyyy_mm_dd=>'2020.10.01'
-,p_release=>'20.2.0.00.20'
+ p_version_yyyy_mm_dd=>'2021.04.15'
+,p_release=>'21.1.6'
 ,p_default_workspace_id=>123132524645685789
 ,p_default_application_id=>666
 ,p_default_id_offset=>0
@@ -25,7 +25,7 @@ wwv_flow_api.create_page(
 '}'))
 ,p_page_template_options=>'#DEFAULT#'
 ,p_last_updated_by=>'QUIZ_DEV'
-,p_last_upd_yyyymmddhh24miss=>'20210801084514'
+,p_last_upd_yyyymmddhh24miss=>'20210922054003'
 );
 wwv_flow_api.create_page_plug(
  p_id=>wwv_flow_api.id(286600257237137941)
